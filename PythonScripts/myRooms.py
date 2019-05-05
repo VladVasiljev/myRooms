68d6a4a197300e7a5914b381adc2ac2dc3933af8
VladVasiljev <vlad.vasiljevs97@gmail.com>
2019-05-05 23:11:52 +0100

    Added emails, gas sensors etc

--- PythonScripts/myRooms.py
+++ PythonScripts/myRooms.py
@@ -3,6 +3,7 @@ import pigpio
 import time
 from firebase import firebase
 import pygame
+import smtplib
 
 firebase = firebase.FirebaseApplication('https://myrooms-2019iot.firebaseio.com/')  # Firebase url
 firebaseURL = 'https://myrooms-2019iot.firebaseio.com/'  # FirebasURL on its own so we can use it throughout the script
@@ -15,16 +16,38 @@ BLUE_PIN = 22  # Using Pin 22- to Blue LED
 pir = MotionSensor(4)
 pi = pigpio.pi()
 
+
+def emailSender():#Method that allows us to send an email
+    username = "123hello2020@gmail.com"#Email adress for the test account
+    password = "dingatding2020"#Password for the test account
+    FROM = "123hello2020@gmail.com"
+    TO = ["123hello2020@gmail.com"]
+    SUBJECT = "Movement Detected"
+    TEXT = "Movement Detected, someone has entered your room"
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print ("Email Sent")       
+    except:
+        print ("Failed to send email")
+        
+
 def set_lights(pin, brightness):
     real_brightness = int(int(brightness) * (float(255) / 255.0))
     pi.set_PWM_dutycycle(pin, real_brightness)
-    
+
+#Method that checks if movement was detected, if so then we flash the lights and play a sound, given that motionDetect = True
 def motion():
-    print("Is intruder alarm set: "+ str(firebase.get(firebaseURL, '/motionDetect')))
-    pir.wait_for_motion()
-    print("Movement Detected: "+str(pir.wait_for_motion()))
-    #motionDetect = True
-    while motionDetect == firebase.get(firebaseURL, '/motionDetect'):
+    print("Is intruder alarm set: "+ str(firebase.get(firebaseURL, '/motionDetect')))#Printing motionDetect,checking if its ture of false 
+    pir.wait_for_motion()#Waiting for motion to be detected
+    print("Movement Detected: "+str(pir.wait_for_motion()))#Printing that motion was detected
+    count = 0 #Setting count to 0
+    while motionDetect == firebase.get(firebaseURL, '/motionDetect'):#Running these lines if motion detected and if the alarm is set
         set_lights(RED_PIN,red)
         time.sleep(0.5)
         set_lights(RED_PIN,0)
@@ -34,6 +57,9 @@ def motion():
         pygame.mixer.init()
         pygame.mixer.music.load("/home/pi/Desktop/1.mp3")
         pygame.mixer.music.play()
+        if count == 0: #If count = 0 then we send a email
+            emailSender()
+            count = 1  #We set count to 1, this prevents the email being sent again, email we be sent again once the alarm is disarmed and armed again
     
 
 while True:
