de9515340cecf976a611b67111dce01dddc640b6
VladVasiljev <vlad.vasiljevs97@gmail.com>
2019-05-10 14:44:44 +0100

    Added comments

--- PythonScripts/receiver.py
+++ PythonScripts/receiver.py
@@ -1,40 +1,52 @@
 import sqlite3 #Importing sqlite3
 from firebase import firebase #Importing python-firebase library
-from datetime import datetime
+from datetime import datetime #Importing datetime
+import time #Importing time
 
 firebase = firebase.FirebaseApplication('https://myrooms-2019iot.firebaseio.com/')  #Pointing to our Firebase through URL
 firebaseURL = 'https://myrooms-2019iot.firebaseio.com/'  # Firebase stored in variable so we can call it any in the file
 
-conn = sqlite3.connect('myRoomsDB.db') 
-c = conn.cursor()
+conn = sqlite3.connect('myRoomsDB.db') #Creating a connection object
+c = conn.cursor() #Creating a cursor object
 
-def create_table():
-    c.execute('CREATE TABLE IF NOT EXISTS motionDetect(value TEXT, date TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS computerRelaySwitch(value TEXT, date TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS mq2sensorValue(value TEXT, date TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS mq2sensorDensity(value TEXT, date TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS mq9sensorValue(value TEXT, date TEXT)')
-    c.execute('CREATE TABLE IF NOT EXISTS mq9sensorDensity(value TEXT, date TEXT)')
+#Method which creates the tables if they don't exist in myRoomsDB.db
+#Each table takes in two values value as BLOB and date as TEXT
+def createTable():
+    c.execute('CREATE TABLE IF NOT EXISTS computerRelaySwitch(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS mq2sensorValue(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS mq2sensorDensity(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS mq9sensorValue(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS mq9sensorDensity(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS roomTemp(value BLOB, date TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS roomHum(value BLOB, date TEXT)')
 
-
+#Method which get values from firebase and then the values are put into tables
 def enterDataToDB():
+    time.sleep(3)
+    
+    #Getting and storing values from firebase
     computerRelaySwitch = firebase.get(firebaseURL, '/computerRelaySwitch')
-    check = computerRelaySwitch
     mq2sensorValue = firebase.get(firebaseURL, '/mq2sensorValue')
     mq2sensorDensity = firebase.get(firebaseURL, '/mq2sensorDensity')
     mq9sensorValue = firebase.get(firebaseURL, '/mq9sensorValue')
     mq9sensorDensity = firebase.get(firebaseURL, '/mq9sensorDensity')
-    date = datetime.now() 
-    c.execute("INSERT INTO motionDetect(value,date)VALUES (?,?)",(motionDetect, date.strftime('%Y-%m-%d %H:%M:%S')))
+    roomTemp = firebase.get(firebaseURL, '/RoomTemperature')
+    roomHum = firebase.get(firebaseURL, '/RoomHumidity')
+    
+    date = datetime.now() #Getting current time and date
+    
+    #Putting the information gathered into our tables
     c.execute("INSERT INTO computerRelaySwitch(value,date)VALUES (?,?)",(computerRelaySwitch, date.strftime('%Y-%m-%d %H:%M:%S')))
     c.execute("INSERT INTO mq2sensorValue(value,date)VALUES (?,?)",(mq2sensorValue, date.strftime('%Y-%m-%d %H:%M:%S')))
     c.execute("INSERT INTO mq2sensorDensity(value,date)VALUES (?,?)",(mq2sensorDensity, date.strftime('%Y-%m-%d %H:%M:%S')))
     c.execute("INSERT INTO mq9sensorValue(value,date)VALUES (?,?)",(mq9sensorValue, date.strftime('%Y-%m-%d %H:%M:%S')))
     c.execute("INSERT INTO mq9sensorDensity(value,date)VALUES (?,?)",(mq9sensorDensity, date.strftime('%Y-%m-%d %H:%M:%S')))
-    conn.commit()
+    c.execute("INSERT INTO roomTemp(value,date)VALUES (?,?)",(roomTemp, date.strftime('%Y-%m-%d %H:%M:%S')))
+    c.execute("INSERT INTO roomHum(value,date)VALUES (?,?)",(roomHum ,date.strftime('%Y-%m-%d %H:%M:%S')))
+    conn.commit() #Saving changes
 
-create_table()
+createTable() #Calling createTable method
 
 
 while True:
-    enterDataToDB()
+    enterDataToDB() #Calling enterDataToDB method
